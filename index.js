42ae62ebbee28e254846d19fd32af2291ccbb901
Lucky <vagyasri2011@gmail.com>
2021-09-21 17:32:53 +0200

    Add arrow funtion to eventlistenertype

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.addEventListener('click', function(){
+//     return "Hello";
+// });
+
+// document.addEventListener('click', () => {
+//     return "Hello";
+// });
+
+document.addEventListener('click', () => 'Hello');
