8e3751d2b4ddd6bbd285a37d3ed09b29a3785c30
Lucky <vagyasri2011@gmail.com>
2021-09-21 18:04:15 +0200

    Add array object

--- index.js
+++ index.js
@@ -7,3 +7,24 @@
 // });
 
 document.addEventListener('click', () => 'Hello');
+// document.addEventListener('click', () => console.log('click'));
+
+// Array Object
+const bio = [
+  {
+    name: 'Lucky',
+    age: 28,
+  },
+  {
+    name: 'Patra',
+    age: 28,
+  },
+];
+
+// document.getElementById('display-books').innerHTML = bio.map(function (girl) {
+//     return girl.name;
+// });
+
+document.getElementById('display-books').innerHTML = bio.map((girl) => girl.name);
+
+// this
